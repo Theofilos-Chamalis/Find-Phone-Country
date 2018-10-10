fd3b913218eb62cd726004df8fe5bc3029171079
Theofilos Chamalis <theofxam@gmail.com>
2018-10-10 22:50:49 +0300

    Initial work on the timeline in the History tab

--- screens/tabs/HistoryTab.js
+++ screens/tabs/HistoryTab.js
@@ -1,10 +1,44 @@
 import { Container, Text, Content, Icon } from 'native-base';
 import React, { Component } from 'react';
 import { StyleSheet, View, StatusBar } from 'react-native';
+import Timeline from 'react-native-timeline-listview';
 
 import { getAllRecords } from '../../db/asyncStorageProvider';
 
-export default class PlanetTab extends Component {
+export default class HistoryTab extends Component {
+  constructor(props) {
+    super(props);
+    this.data = [
+      {
+        time: '04/06\n09:00',
+        title: 'Archery Training',
+        description:
+          'The Beginner Archery and Beginner Crossbow course does not require you to bring any equipment, since everything you need will be provided for the course. ',
+        icon: require('../../assets/globenphone.png')
+      },
+      {
+        time: '29/05\n10:45',
+        title: 'Play Badminton',
+        description:
+          'Badminton is a racquet sport played using racquets to hit a shuttlecock across a net.',
+        icon: require('../../assets/globenphone.png')
+      },
+      {
+        time: '28/05\n14:00',
+        title: 'Watch Soccer',
+        description:
+          'Team sport played between two teams of eleven players with a spherical ball. ',
+        icon: require('../../assets/globenphone.png')
+      },
+      {
+        time: '27/05\n16:30',
+        title: 'Go to Fitness center',
+        description: 'Look out for the Best Gym & Fitness Centers around me :)',
+        icon: require('../../assets/globenphone.png')
+      }
+    ];
+  }
+
   static navigationOptions = {
     tabBarIcon: ({ tintColor }) => {
       return <Icon name="ios-time-outline" style={{ color: tintColor }} />;
@@ -24,31 +58,39 @@ export default class PlanetTab extends Component {
 
   render() {
     return (
-      <Container style={styles.containerStyle}>
-        <StatusBar backgroundColor="#B71C1C" />
-        <Content>
-          <View style={styles.noUsersView}>
-            <Text style={styles.noUsersText}> This is the history tab</Text>
-          </View>
-        </Content>
-      </Container>
+      <View style={styles.container}>
+        <Timeline
+          data={this.data}
+          circleSize={20}
+          iconStyle={{ marginTop: 25 }}
+          circleColor="#212121"
+          lineColor="#C62828"
+          timeContainerStyle={{ minWidth: 52, marginTop: 0 }}
+          timeStyle={{
+            textAlign: 'center',
+            backgroundColor: '#306BAC',
+            color: 'white',
+            padding: 5,
+            borderRadius: 13
+          }}
+          titleStyle={{ color: '#ffffff' }}
+          descriptionStyle={{ color: '#EAE6E5' }}
+          options={{
+            style: { paddingTop: 5 }
+          }}
+          innerCircle={'icon'}
+        />
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  containerStyle: {
+  container: {
     flex: 1,
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 10,
     backgroundColor: '#212121'
-  },
-  noUsersText: {
-    fontSize: 26,
-    fontWeight: 'bold',
-    color: 'white'
-  },
-  noUsersView: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'row'
   }
 });
