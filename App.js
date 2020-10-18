7d188c881adf2687b886e237d57b0e697220e1a7
Theofilos Chamalis <theofxam@gmail.com>
2020-10-18 11:37:14 +0300

    Disable console.log in production

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react';
-import { createAppContainer } from 'react-navigation';
+import React, {Component} from 'react';
+import {createAppContainer} from 'react-navigation';
 import RootStackNavigator from './src/navigation/navigators/RootStackNavigator';
 
 const ApplicationContainer = createAppContainer(RootStackNavigator);
 
+// Disable console logs in production
+if (!__DEV__) {
+    console.log = () => {
+    };
+}
+
 export default class App extends Component {
-  render() {
-    return <ApplicationContainer />;
-  }
+    render() {
+        return <ApplicationContainer/>;
+    }
 }
